da9c57a18b5cbcd2bfb9811dbae6c7ecfcd75d17
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-17 13:12:25 -0700

    learn react routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import About from './About'
+import Login from './Login'
+import Navbar from './Navbar'
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
+
+
+// Step 2. Changed to have router coordinate what is displayed
+
+ReactDOM.render((
+  <Router>
+    
+    <div> 
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/" component={Home} />  */}
+      {/* <Route path="/" render={() => <h1>Home!</h1>} /> */}
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} /> 
     </div>
-  );
-};
 
-ReactDOM.render(
-  <Home />,
+  </Router>),
   document.getElementById('root')
 );
